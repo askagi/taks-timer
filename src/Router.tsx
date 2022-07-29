8643c5cce2007a10c9ee4405150818b7aa03ead3
José Costa <jose@askagi.bbrouter>
2022-07-29 11:32:12 -0300

    create default layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
+import { DefaultLeayout } from './layouts/DefaultLayout'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/">
+      <Route path="/" element={<DefaultLeayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/history" element={<History />} />
       </Route>
-      <Route path="/history" element={<History />} />
     </Routes>
   )
 }
--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,3 +1,13 @@
 export function History() {
-  return <h1>History</h1>
+  return (
+    <div>
+      <h1>History</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis pariatur,
+        vero dicta optio similique saepe. Ea ratione maiores repellat, voluptas
+        ex consequuntur commodi exercitationem sit totam, ducimus earum! Odio,
+        sequi?
+      </p>
+    </div>
+  )
 }
